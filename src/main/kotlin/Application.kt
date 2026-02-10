50313e0e36d5be9e7f121d0670b3e4c2303450dd
achcyano <6617260@qq.com>
2026-02-10 17:34:57 +0800

    从env动态获取port

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,12 +1,18 @@
 package me.hztcm
 
+import io.github.cdimascio.dotenv.dotenv
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
+val dotenv = dotenv()
 fun main() {
-    embeddedServer(Netty, port = 8808, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
+    embeddedServer(
+        factory = Netty,
+        port = dotenv["KTOR_HTTP_PORT"]?.toInt() ?: 8808,
+        host = "0.0.0.0",
+        module = Application::module
+    ).start(wait = true)
 }
 
 fun Application.module() {
