a38f47040dbf0d577a484e42848426ee9db7e8b4
achcyano <6617260@qq.com>
2026-02-10 17:26:41 +0800

    添加dotenv依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
     implementation(libs.ktor.server.core)
+    implementation(libs.dotenv.kotlin)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 }
